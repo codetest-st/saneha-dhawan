66a90f8cf42c064886769c35d5f494df5d79b354
codetest-st <yash38daga@gmail.com>
2026-07-03 16:34:13 +0530

    program8

---
+++ p8.py
@@ -0,0 +1,5 @@
+principal=float(input("enter principal amount: "))
+rate=float(input("enter rate of interest: "))
+time=float(input("enter time (in year): "))
+simple_interest= (principal*rate*time)/100
+print(f"simple interest is {simple_interest}")
