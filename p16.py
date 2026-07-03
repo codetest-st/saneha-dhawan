865edb3429f229e17369bcdb9a0631329ced7c0a
codetest-st <yash38daga@gmail.com>
2026-07-03 17:24:32 +0530

    program16

---
+++ p16.py
@@ -0,0 +1,8 @@
+a= int(input("enter first number: "))
+b= int(input("enter second number"))
+a=a+b
+b=a-b
+a=a-b
+print(f"after swapping: ")
+print(f"first number is {a}")
+print(f"second number is {b}")
