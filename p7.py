23576eb02fb029e6cc4810dd1ed45a1824b9fbcf
codetest-st <yash38daga@gmail.com>
2026-07-03 17:00:24 +0530

    program7

---
+++ p7.py
@@ -0,0 +1,9 @@
+sub1= float(input("enter the mark of subject1: "))
+sub2= float(input("enter the mark of subject2: "))
+sub3= float(input("enter the mark of subject3: "))
+sub4= float(input("enter the mark of subject4: "))
+sub5= float(input("enter the mark of subject5: "))
+total= sub1+sub2+sub3+sub4+sub5
+percentage= total/5
+print(f"total marks is {total}")
+print(f"percentage is {percentage}")
